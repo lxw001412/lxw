40c1f86ff1de15d6caaf02b3101c94ee79f83915
lxw001412 <3150797476@qq.com>
2023-03-24 15:50:38 +0800

    忽略
    测试
    
    3rd
    
    3rd
    
    3rd

---
+++ 80press/CJson.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <string>
+#include "json/json.h"
+#include "common.h"
+class JsonData
+{
+public:
+	JsonData();
+	void Init(std::string sn,std::string addr,int streamid = -1,bool Start = true);
+	~JsonData();
+	std::string Dump();
+	operator Json::Value() { if(!m_root.isNull())return m_root;}
+	JsonData& operator = (const Json::Value& root);
+public:
+	int m_msgid;
+	Json::Value m_root;
+	Json::StreamWriterBuilder writerBuilder;
+};
+
---
+++ 80press/CLog.h
@@ -0,0 +1,51 @@
+﻿#ifndef _CLOG_H_
+#define _CLOG_H_
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+class CLog
+{
+public:
+	static std::shared_ptr<spdlog::logger> GetInstace();
+private:
+	CLog();
+	~CLog();
+protected:
+	static std::shared_ptr<spdlog::logger> GetLogger();
+	static void releaseInstance() {
+		if (m_log != NULL) {
+			CLog* tmp = m_log;
+			m_log = NULL;
+			delete tmp;
+		}
+		if (m_logger != NULL) {
+			spdlog::drop_all();
+		}
+	}
+private:
+	static CLog* m_log;
+	static std::shared_ptr<spdlog::logger> m_logger;
+	class CHelper {
+	public:
+		CHelper() {
+			CLog::GetInstace();
+		}
+		~CHelper() {
+			CLog::releaseInstance();
+		}
+	};
+	static CHelper m_helper;
+};
+
+//#ifdef DEBUG
+//debug
+#define LOG_D(...) SPDLOG_LOGGER_CALL(CLog::GetInstace(), spdlog::level::debug, __VA_ARGS__)
+//info
+#define LOG_I(...) SPDLOG_LOGGER_CALL(CLog::GetInstace(), spdlog::level::info, __VA_ARGS__)
+//warn
+#define LOG_W(...) SPDLOG_LOGGER_CALL(CLog::GetInstace(), spdlog::level::warn, __VA_ARGS__)
+//error
+#define LOG_E(...) SPDLOG_LOGGER_CALL(CLog::GetInstace(), spdlog::level::err, __VA_ARGS__)
+//#endif // DEBUG
+
+#endif //_COMMON_H_
+
